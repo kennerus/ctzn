41bd02f7a9ab7ad179810989ea4109a112eb5f36
dmitryi <dmitryi.zavadskyi@yandex.ru>
2018-04-11 17:02:13 +0300

    main page

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function() {
+	$('.js_tagsSlider').slick({
+		infinite: false,
+		variableWidth: true,
+		responsive: [
+			{
+				breakpoint: 1440,
+				settings: {
+					arrows: false
+				}
+			}
+		]
+	})
+})
