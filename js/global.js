3a02da1b5ffd9e3eecaf7650edeff3673151bd88
dmitryi <dmitryi.zavadskyi@yandex.ru>
2018-04-16 13:47:06 +0300

    header scroll

--- js/global.js
+++ js/global.js
@@ -5,15 +5,41 @@ $(function() {
 	})
 })
 
-var flag = true;
-$(document).scroll(function() {
-	if($(document).scrollTop() >= 300 && flag) {
-		flag = false;
-		$('.js_header').addClass('header_border');
-	}
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('.js_header').outerHeight();
 
-	if($(document).scrollTop() < 300 && flag == false) {
-		flag = true;
-		$('.js_header').removeClass('header_border');
-	}
-})
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('.js_header').addClass('header__nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('.js_header').removeClass('header__nav-up');
+        }
+    }
+    
+    lastScrollTop = st;
+}
