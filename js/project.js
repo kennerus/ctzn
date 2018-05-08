bd666a4764bd80d9458259696389c8f56fde71d0
dmitryi <dmitryi.zavadskyi@yandex.ru>
2018-05-08 13:56:31 +0300

    fixes

--- js/project.js
+++ js/project.js
@@ -1,6 +1,5 @@
 $(function() {
 	$(document).on('click', '.js_slideText', function() {
-		$('.hidden').removeClass('hidden').css('max-height', '9999px').addClass('js_activeText');
 		$('.hidden_flex').removeClass('hidden_flex').css('max-height', '9999px').addClass('js_activeFlex');
 		$('.js_hideText').css('display', 'flex');
 		$(this).hide(0).addClass('hidden_flex');
@@ -11,7 +10,6 @@ $(function() {
 	$(document).on('click', '.js_hideText', function() {
 		$('.js_slideText').show(0).removeClass('hidden_flex').css('display', 'flex');
 		$('.js_hideText').css('display', 'none');
-		$('.js_activeText').css('max-height', '0').removeClass('js_activeText').addClass('hidden');
 		$('.js_activeFlex').css('max-height', '0').removeClass('js_activeFlex').addClass('hidden_flex');
 		$('.js_container').removeClass('container_row').addClass('container_wrap');
 	});
