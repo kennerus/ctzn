f1f4c029e0abfdd845dfb1f6d5621adf2fdcac43
dmitryi <dmitryi.zavadskyi@yandex.ru>
2018-05-16 10:27:48 +0300

    # Изменение страницы case (убрать ссылки обёртки) # Фикс текстового блока # Карточки авторов

--- js/global.js
+++ js/global.js
@@ -1,70 +1,70 @@
-$(function() {
-    if ($('#contacts').length > 0) {
-        $('.js_contacts').addClass('active-item');
+$(function () {
+  if ($('#contacts').length > 0) {
+    $('.js_contacts').addClass('active-item');
+  }
+
+  if ($('#about').length > 0) {
+    $('.js_about').addClass('active-item');
+  }
+
+  // mob menu
+  $(document).on('click', '.js_openNav', function () {
+    $(this).toggleClass('header__hamburger_active');
+    $('.js_nav').toggleClass('header__nav-mob_active');
+    $('body').toggleClass('overflow-hidden');
+    $('.js_header').toggleClass('header_border');
+  })
+
+  // dropdown menu
+  $(document).on('click', '.js_dropdownBtn', function () {
+    $('.js_dropdownBtn').removeClass('active-item');
+    $('.js_dropdownArrow').removeClass('header__rotate-arrow');
+    if ($(this).next('.js_headerDropdown').hasClass('header__dropdown_opened')) {
+      $('.js_headerDropdown').removeClass('header__dropdown_opened');
     }
-
-    if ($('#about').length > 0) {
-        $('.js_about').addClass('active-item');
+    else {
+      $('.js_headerDropdown').removeClass('header__dropdown_opened');
+      $(this).next('.js_headerDropdown').addClass('header__dropdown_opened');
+      $(this).addClass('active-item');
+      $(this).find('.js_dropdownArrow').addClass('header__rotate-arrow');
     }
+  });
 
-    // mob menu
-    $(document).on('click', '.js_openNav', function() {
-        $(this).toggleClass('header__hamburger_active');
-        $('.js_nav').toggleClass('header__nav-mob_active');
-        $('body').toggleClass('overflow-hidden');
-        $('.js_header').toggleClass('header_border');
-    })
+  $(document).on('mouseleave', '.js_headerDropdown', function () {
+    if ($(window).width() > 1140) {
+      $(this).removeClass('header__dropdown_opened');
+      $('.js_dropdownBtn').removeClass('active-item');
+      $('.js_dropdownArrow').removeClass('header__rotate-arrow');
+    }
+  });
 
-    // dropdown menu
-    $(document).on('click', '.js_dropdownBtn', function() {
-        $('.js_dropdownBtn').removeClass('active-item');
-        $('.js_dropdownArrow').removeClass('header__rotate-arrow');
-        if($(this).next('.js_headerDropdown').hasClass('header__dropdown_opened')) {
-            $('.js_headerDropdown').removeClass('header__dropdown_opened');
-        }
-        else {
-            $('.js_headerDropdown').removeClass('header__dropdown_opened');
-            $(this).next('.js_headerDropdown').addClass('header__dropdown_opened');
-            $(this).addClass('active-item');
-            $(this).find('.js_dropdownArrow').addClass('header__rotate-arrow');
-        }
+  // text truncate
+  if ($(window).width() < 1440 && $(window).width() > 750) {
+    $('.text-block p').dotdotdot({
+      ellipsis: "\u2026 ",
+      height: 140
     });
+  }
 
-    $(document).on('mouseleave', '.js_headerDropdown', function() {
-        if ($(window).width() > 1140) {
-            $(this).removeClass('header__dropdown_opened');
-            $('.js_dropdownBtn').removeClass('active-item');
-            $('.js_dropdownArrow').removeClass('header__rotate-arrow');
-        }
+  if ($(window).width() < 750) {
+    $('.text-block p').dotdotdot({
+      ellipsis: "\u2026 ",
+      height: 100
     });
+  }
 
-    // text truncate
-    if ($(window).width() < 1440 && $(window).width() > 750) {
-        $('.text-block p').dotdotdot({
-            ellipsis: "\u2026 ",
-            height: 140
-       });
-    }
+  // hide-show custom cursor on header elements
+  $(document).on('mouseenter', '.js_cursorPointer', function () {
+    $(this).css('cursor', 'pointer');
+    $(this).find('*').css('cursor', 'pointer');
+    $('#cursor').css('display', 'none');
+  })
 
-    if ($(window).width() < 750) {
-        $('.text-block p').dotdotdot({
-            ellipsis: "\u2026 ",
-            height: 100
-       });
-    }
-
-    // hide-show custom cursor on header elements
-    $(document).on('mouseenter', '.js_cursorPointer', function() {
-        $(this).css('cursor', 'pointer');
-        $(this).find('*').css('cursor', 'pointer');
-        $('#cursor').css('display', 'none');
-    })
-
-    $(document).on('mouseleave', '.js_cursorPointer', function() {
-        $(this).css('cursor', 'none');
-        $(this).find('*').css('cursor', 'none');
-        $('#cursor').css('display', 'flex');
-    })
+  $(document).on('mouseleave', '.js_cursorPointer', function () {
+    $(this).css('cursor', 'none');
+    $(this).find('*').css('cursor', 'none');
+    $('#cursor').css('display', 'flex');
+  })
 })
 
 // Hide Header on on scroll down
@@ -77,124 +77,124 @@ var yMousePos = 0;
 var lastScrolledLeft = 0;
 var lastScrolledTop = 0;
 
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
+setInterval(function () {
+  if (didScroll) {
+    hasScrolled();
+    didScroll = false;
+  }
 }, 5);
 
 function hasScrolled() {
-    var st = $(this).scrollTop();
-    
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-    
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        $('.js_header').addClass('header__nav-up');
-    } 
-    else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('.js_header').removeClass('header__nav-up');
-        }
-    }
+  var st = $(this).scrollTop();
 
-    if($(window).scrollTop() > delta) {
-        $('.js_header').addClass('header_border');
+  // Make sure they scroll more than delta
+  if (Math.abs(lastScrollTop - st) <= delta)
+    return;
+
+  // If they scrolled down and are past the navbar, add class .nav-up.
+  // This is necessary so you never see what is "behind" the navbar.
+  if (st > lastScrollTop && st > navbarHeight) {
+    // Scroll Down
+    $('.js_header').addClass('header__nav-up');
+  }
+  else {
+    // Scroll Up
+    if (st + $(window).height() < $(document).height()) {
+      $('.js_header').removeClass('header__nav-up');
     }
-    else {
-        $('.js_header').removeClass('header_border');
-    }
-    
-    lastScrollTop = st;
+  }
+
+  if ($(window).scrollTop() > delta) {
+    $('.js_header').addClass('header_border');
+  }
+  else {
+    $('.js_header').removeClass('header_border');
+  }
+
+  lastScrollTop = st;
 }
 
-$(window).scroll(function(event){
-    didScroll = true;
+$(window).scroll(function (event) {
+  didScroll = true;
 
-    if($('.js_hideText').length > 0 && $('.js_hideText').hasClass('js_activeFlex')) {
-        var btnClose = $('.js_hideText');
-        var main = $('.js_stopScrollCross');
-        var mainPos = main.offset();
-        var btnCloseOffset = btnClose.offset();
-        var btnCloseBot = btnCloseOffset.top + btnClose.height();
-        //положение крестика при скролле
-        if($(window).width() > 1140) {
-            if (mainPos.top - $(this).scrollTop() > 50 && $(this).scrollTop() > 125) {
-                btnClose.css({
-                    top: '10px',
-                    position: 'fixed',
-                })
-            }
-            else if (mainPos.top - 50 <= btnCloseBot) {
-                btnClose.css({
-                    position: 'absolute',
-                    top: (mainPos.top - 185) + 'px'
-                });
-            }
-        }
-        if($(window).width() <= 1140) {
-            if (mainPos.top - $(this).scrollTop() > 100 && $(this).scrollTop() > 80) {
-                btnClose.css({
-                    top: '10px',
-                    position: 'fixed',
-                })
-            }
-            else if (mainPos.top - 50 <= btnCloseBot) {
-                btnClose.css({
-                    position: 'absolute',
-                    top: (mainPos.top - 125) + 'px'
-                });
-            }
-        }
-        //положение крестика когда доскроллил до верха
-        if ($(this).scrollTop() < 125 && $(window).width() > 1140) {
-             $('.js_hideText').css({
-                position: 'absolute',
-                top: 'auto'
-            });
-        }
-        if ($(this).scrollTop() < 80 && $(window).width() <= 1140) {
-             $('.js_hideText').css({
-                position: 'absolute',
-                top: 'auto'
-            });
-        }
-
+  if ($('.js_hideText').length > 0 && $('.js_hideText').hasClass('js_activeFlex')) {
+    var btnClose = $('.js_hideText');
+    var main = $('.js_stopScrollCross');
+    var mainPos = main.offset();
+    var btnCloseOffset = btnClose.offset();
+    var btnCloseBot = btnCloseOffset.top + btnClose.height();
+    //положение крестика при скролле
+    if ($(window).width() > 1140) {
+      if (mainPos.top - $(this).scrollTop() > 50 && $(this).scrollTop() > 125) {
+        btnClose.css({
+          top: '10px',
+          position: 'fixed',
+        })
+      }
+      else if (mainPos.top - 50 <= btnCloseBot) {
+        btnClose.css({
+          position: 'absolute',
+          top: (mainPos.top - 185) + 'px'
+        });
+      }
     }
+    if ($(window).width() <= 1140) {
+      if (mainPos.top - $(this).scrollTop() > 100 && $(this).scrollTop() > 80) {
+        btnClose.css({
+          top: '10px',
+          position: 'fixed',
+        })
+      }
+      else if (mainPos.top - 50 <= btnCloseBot) {
+        btnClose.css({
+          position: 'absolute',
+          top: (mainPos.top - 125) + 'px'
+        });
+      }
+    }
+    //положение крестика когда доскроллил до верха
+    if ($(this).scrollTop() < 125 && $(window).width() > 1140) {
+      $('.js_hideText').css({
+        position: 'absolute',
+        top: 'auto'
+      });
+    }
+    if ($(this).scrollTop() < 80 && $(window).width() <= 1140) {
+      $('.js_hideText').css({
+        position: 'absolute',
+        top: 'auto'
+      });
+    }
+
+  }
 });
 
-$(function() {
-    var block = $('.js_removeTablet .js_tabletBlock');
-    var main =  $('.main');
-    var blocksArr = [];
-    if ($(window).width() <= 1140 && $(window).width() > 750) {
-        main.append('<div class="container container_tablet"></div>');
+$(function () {
+  var block = $('.js_removeTablet .js_tabletBlock');
+  var main = $('.main');
+  var blocksArr = [];
+  if ($(window).width() <= 1140 && $(window).width() > 750) {
+    main.append('<div class="container container_tablet"></div>');
 
-        block.each(function() {
-            $(this).appendTo('.container_tablet');
-            if (!$(this).hasClass('block_biggest')) {
-                blocksArr.push($(this))
-            }
-        });
+    block.each(function () {
+      $(this).appendTo('.container_tablet');
+      if (!$(this).hasClass('block_biggest')) {
+        blocksArr.push($(this))
+      }
+    });
 
-        $('.js_removeTablet').remove();
+    $('.js_removeTablet').remove();
 
-        for (var i = 0; i < blocksArr.length; i++) {
-            if (i % 2 == 0) {
-                blocksArr[i].css({
-                    marginRight: '50px',
-                })
-            } else {
-                blocksArr[i].css({
-                    marginRight: '0',
-                })
-            }
-        }
+    for (var i = 0; i < blocksArr.length; i++) {
+      if (i % 2 == 0) {
+        blocksArr[i].css({
+          marginRight: '50px',
+        })
+      } else {
+        blocksArr[i].css({
+          marginRight: '0',
+        })
+      }
     }
+  }
 })
--- js/main.js
+++ js/main.js
@@ -1,167 +1,167 @@
-$(function() {
-    var block = document.querySelectorAll('.js_block');
-    var cursorId = document.getElementById('cursor');
-    var cursorType = 'point';
+$(function () {
+  var block = document.querySelectorAll('.js_block');
+  var cursorId = document.getElementById('cursor');
+  var cursorType = 'point';
 
-    // set our custom cursor position
-    function setDefaultCursor(coordX, coordY, cursor) {
-        if($(window).width() > 1140) {
-            cursorId.style.top = coordY + 'px';
-            cursorId.style.left = coordX + 'px';
-        }
-        if(cursor === 'eye') {
-            $('.cursor').addClass('cursor_active');
-            $('.cursor__eye').css('display', 'block');
-            $('.cursor__text').css('display', 'block');
-        }
-        else {
-            $('.cursor__eye').css({
-                display: 'none',
-                transform: '0'
-            });
-            $('.cursor__text').css('display', 'none');
-            $('.cursor').removeClass('cursor_active');
-        }
+  // set our custom cursor position
+  function setDefaultCursor(coordX, coordY, cursor) {
+    if ($(window).width() > 1140) {
+      cursorId.style.top = coordY + 'px';
+      cursorId.style.left = coordX + 'px';
     }
-
-    // get current cursor position
-    function getCursorPosition(event) {
-        xMousePos = event.pageX;
-        yMousePos = event.pageY - $(document).scrollTop();
-        // window.status = "x = " + xMousePos + " y = " + yMousePos;
-        setDefaultCursor(xMousePos, yMousePos, cursorType);
+    if (cursor === 'eye') {
+      $('.cursor').addClass('cursor_active');
+      $('.cursor__eye').css('display', 'block');
+      $('.cursor__text').css('display', 'block');
     }
-
-    function moveEyes(event, _this) {
-        var title = _this.querySelector('h2');
+    else {
+      $('.cursor__eye').css({
+        display: 'none',
+        transform: '0'
+      });
+      $('.cursor__text').css('display', 'none');
+      $('.cursor').removeClass('cursor_active');
     }
+  }
 
-    function calculateWidth(currentId, blockSelector) {
-        var current = document.getElementById(currentId);
-        var parent = current.parentElement;
-        var blocks = current.querySelectorAll(blockSelector);
-        var blocksWidth = 0;
-        for(var i = 0; i < blocks.length; i++) {
-            blocksWidth += blocks[i].offsetWidth;
-        }
-        current.style.width = blocksWidth + 'px';
-        return blocksWidth;
+  // get current cursor position
+  function getCursorPosition(event) {
+    xMousePos = event.pageX;
+    yMousePos = event.pageY - $(document).scrollTop();
+    // window.status = "x = " + xMousePos + " y = " + yMousePos;
+    setDefaultCursor(xMousePos, yMousePos, cursorType);
+  }
+
+  function moveEyes(event, _this) {
+    var title = _this.querySelector('h2');
+  }
+
+  function calculateWidth(currentId, blockSelector) {
+    var current = document.getElementById(currentId);
+    var parent = current.parentElement;
+    var blocks = current.querySelectorAll(blockSelector);
+    var blocksWidth = 0;
+    for (var i = 0; i < blocks.length; i++) {
+      blocksWidth += blocks[i].offsetWidth;
     }
+    current.style.width = blocksWidth + 'px';
+    return blocksWidth;
+  }
 
-    if($(window).width() > 1140) {
-        calculateWidth('type', 'div');
-        calculateWidth('company', 'div');
+  if ($(window).width() > 1140) {
+    calculateWidth('type', 'div');
+    calculateWidth('company', 'div');
+  }
+
+  document.querySelector('#cont').onmousemove = function (event) {
+    if ($(window).width() > 1140) {
+      var moveX = event.clientX - this.getBoundingClientRect().left;
+      moveSlider(event, 'type', 1, moveX);
+      moveSlider(event, 'company', 1, moveX);
     }
+  }
 
-    document.querySelector('#cont').onmousemove = function(event) {
-        if($(window).width() > 1140) {
-            var moveX = event.clientX - this.getBoundingClientRect().left;
-            moveSlider(event, 'type', 1, moveX);
-            moveSlider(event, 'company', 1, moveX);
-        }
+  $('.desc__tags a').hover(
+    function (event) {
+      $(this).css({
+        color: '#fff',
+        backgroundColor: '#000',
+        opacity: '1'
+      })
+
+    },
+    function (event) {
+      console.log('out');
+      console.log(event);
+      console.log($(this)[0])
+      $(this).css({
+        color: '#000',
+        backgroundColor: '#fff',
+        opacity: '0.8'
+      })
+      if (event.relatedTarget === $('.cursor')[0] && event.delegateTarget === $(this)[0]) {
+        $(this).css({
+          color: '#fff',
+          backgroundColor: '#000',
+          opacity: '1'
+        })
+      }
     }
+  )
 
-    $('.desc__tags a').hover(
-        function(event) {
-            $(this).css({
-                color: '#fff',
-                backgroundColor: '#000',
-                opacity: '1'
-            })
-            
+  function moveSlider(event, sliderId, multiplier, moveX) {
+    sliderId = document.getElementById(sliderId);
+    var sliderWidth = sliderId.offsetWidth;
+    var container = document.querySelector('.desc__tags');
+    var containerWidth = container.offsetWidth;
+    var mouseMove = (moveX / containerWidth) * 100;
+    var sliderOffset = ((sliderWidth - containerWidth) / 100) * mouseMove;
+
+    if (document.body.clientWidth >= 1140 && sliderId) {
+      sliderId.style.marginLeft = '-' + sliderOffset + 'px';
+    }
+  }
+
+  $('.main').mousemove(function (event) {
+    var parent = event.target.offsetParent;
+    var title = event.target.offsetParent.offsetParent;
+    if (title === null) {
+      cursorType = 'point';
+      $('.cursor').css('transition', '0.2s');
+
+      setTimeout(
+        function () {
+          $('.cursor').css('transition', 'none');
         },
-        function(event) {
-            console.log('out');
-            console.log(event);
-            console.log($(this)[0])
-            $(this).css({
-                color: '#000',
-                backgroundColor: '#fff',
-                opacity: '0.8'
-            })
-            if (event.relatedTarget === $('.cursor')[0] && event.delegateTarget === $(this)[0]) {
-                $(this).css({
-                    color: '#fff',
-                    backgroundColor: '#000',
-                    opacity: '1'
-                })
-            }
-        }
-    )
-
-    function moveSlider(event, sliderId, multiplier, moveX) {
-        sliderId = document.getElementById(sliderId);
-        var sliderWidth = sliderId.offsetWidth;
-        var container = document.querySelector('.desc__tags');
-        var containerWidth = container.offsetWidth;
-        var mouseMove = (moveX / containerWidth) * 100;
-        var sliderOffset = ((sliderWidth - containerWidth) / 100) * mouseMove;
-
-        if(document.body.clientWidth >= 1140 && sliderId) {
-            sliderId.style.marginLeft = '-' + sliderOffset + 'px';
-        }
+        200)
     }
+    if (parent.classList.contains('js_block') || title.classList.contains('js_block')) {
+      cursorType = 'eye';
+      $('.cursor').css('transition', '0.2s');
+      $('body').attr('data-time', $(parent).attr('data-time'));
+      var dataTime = $('body').attr('data-time');
+      $('.cursor__text').html(dataTime + ' мин.');
 
-    $('.main').mousemove(function(event) {
-        var parent = event.target.offsetParent;
-        var title = event.target.offsetParent.offsetParent;
-        if (title === null) {
-            cursorType = 'point';
-            $('.cursor').css('transition', '0.2s');
+      setTimeout(
+        function () {
+          $('.cursor').css('transition', 'none');
+        },
+        200)
+    }
+    else {
+      cursorType = 'point';
+      $('.cursor').css('transition', '0.2s');
 
-            setTimeout(
-                function() {
-                    $('.cursor').css('transition', 'none');
-                },
-                200)
-        }
-        if (parent.classList.contains('js_block') || title.classList.contains('js_block')) {
-            cursorType = 'eye';
-            $('.cursor').css('transition', '0.2s');
-            $('body').attr('data-time', $(parent).attr('data-time'));
-            var dataTime = $('body').attr('data-time');
-            $('.cursor__text').html(dataTime + ' мин.');
-
-            setTimeout(
-                function() {
-                    $('.cursor').css('transition', 'none');
-                },
-                200)
-        }
-        else {
-            cursorType = 'point';
-            $('.cursor').css('transition', '0.2s');
-
-            setTimeout(
-                function() {
-                    $('.cursor').css('transition', 'none');
-                },
-                200)
-        }
-    })
-    $(".js_block").mousemove(function(event) {
-        var eye = $(".cursor__eye");
-        var title = $(this).find('h2 span');
-        if (eye.length > 0) {
-            var eyePos = eye.offset();
-            var x = (eyePos.left) + (eye.width() / 2);
-            var y = (eyePos.top) + (eye.height() / 2);
-        }
-        var titleWidth = title.width();
-        var titleHeight = title.height();
-        var title = title.offset();
-        var titleX = title.left + titleWidth / 2;
-        var titleY = title.top + titleHeight / 2;
-        var rad = Math.atan2(titleX - x, titleY - y);
-        // нельзя менять математическую формулу! \/
-        var rot = (rad * (180 / Math.PI) * -1) + 180;
-        eye.css({
-          'transform': 'rotate(' + rot + 'deg)'
-        });
+      setTimeout(
+        function () {
+          $('.cursor').css('transition', 'none');
+        },
+        200)
+    }
+  })
+  $(".js_block").mousemove(function (event) {
+    var eye = $(".cursor__eye");
+    var title = $(this).find('h2 span');
+    if (eye.length > 0) {
+      var eyePos = eye.offset();
+      var x = (eyePos.left) + (eye.width() / 2);
+      var y = (eyePos.top) + (eye.height() / 2);
+    }
+    var titleWidth = title.width();
+    var titleHeight = title.height();
+    var title = title.offset();
+    var titleX = title.left + titleWidth / 2;
+    var titleY = title.top + titleHeight / 2;
+    var rad = Math.atan2(titleX - x, titleY - y);
+    // нельзя менять математическую формулу! \/
+    var rot = (rad * (180 / Math.PI) * -1) + 180;
+    eye.css({
+      'transform': 'rotate(' + rot + 'deg)'
     });
+  });
 
 
-    document.onmousemove = function(event) {
-        getCursorPosition(event);
-    }
+  document.onmousemove = function (event) {
+    getCursorPosition(event);
+  }
 })
